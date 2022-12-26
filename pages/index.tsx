9519a55503413d20a1b9c7be036555e6eec2f995
SangWonKo <swko1017@naver.com>
2022-12-27 00:40:20 +0900

    tailwindcss & emotion 설정

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,5 @@
+import Button from '@components/Button'
+import { css } from '@emotion/react'
 import type { NextPage } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
@@ -48,14 +50,33 @@ const Home: NextPage = () => {
           Welcome to <a href="https://nextjs.org">Next.js!</a>
         </h1>
 
-        <input ref={inputRef} type="text" placeholder="name" />
-        <button onClick={handleClick}>Add Jacket</button>
+        <input
+          className="placeholder:italic placeholder:text-slate-400 block bg-white w-96 border border-slate-300 rounded-md py-2 pl-3 pr-3 shadow-sm focus:outline-none focus:border-sky-500 focus:ring-sky-500 focus:ring-1 sm:text-sm"
+          ref={inputRef}
+          type="text"
+          placeholder="name"
+        />
+        <button
+          css={css`
+            background-color: hotpink;
+            padding: 16px;
+            border-radius: 8px;
+          `}
+          onClick={handleClick}
+        >
+          Add Jacket
+        </button>
+        <Button onClick={handleClick}>Add Jacket 2</Button>
 
         <div>
           <p>Product List</p>
-          {products && products.map(item => (
-            <div key={item.id}>{item.name}<span>{item.createdAt}</span></div>
-          ))}
+          {products &&
+            products.map((item) => (
+              <div key={item.id}>
+                {item.name}
+                <span>{item.createdAt}</span>
+              </div>
+            ))}
           {/* {products &&
             products.map((item) => (
               <div key={item.id}>
