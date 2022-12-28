b531900ea7b1b7f48a65581aac1f490b79d9ec0e
SangWonKo <swko1017@naver.com>
2022-12-29 02:09:19 +0900

    robots.txt & sitemap 설정

---
+++ next-sitemap.config.js
@@ -0,0 +1,6 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+  siteUrl: 'https://nextjs-commerce.vercel.app',
+  generateRobotsTxt: true, // (optional)
+  // ...other options
+}
